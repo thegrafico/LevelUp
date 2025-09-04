58728f29c2f9e03c6f4ab4c2943b9ea90789612a
Raul Pichardo <raul022107@gmail.com>
2025-09-04 17:56:30 -0400

    Initial Commit

---
+++ LevelUp/LevelUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUpApp.swift
+//  LevelUp
+//
+//  Created by Raúl Pichardo Avalo on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct LevelUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4d40fb454cebbf1d52bcdb956577f8cb94f3ed1e
Raul Pichardo <raul022107@gmail.com>
2025-10-18 15:44:33 -0400

    Canceled friend request is working

---
+++ LevelUp/Components/Auth/AsyncConfirmationModal.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncConfirmationModal.swift
+//  LevelUp
+//
+//  Created by Raúl Pichardo Avalo on 10/18/25.
+//
+
+import SwiftUI
+
+struct AsyncConfirmationModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncConfirmationModal()
+}
